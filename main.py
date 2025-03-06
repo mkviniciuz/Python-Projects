20d5282740365a5c8fe129d5a758698d32becb1c
mkviniciuz <54824074+mkviniciuz@users.noreply.github.com>
2025-03-06 17:58:56 -0300

    Criação dos metodos dos monstros
    Foi criado o metodo para exibição dos status dos monstros e a criação de uma lista de monstros com status aleatorios

--- main.py
+++ main.py
@@ -27,33 +27,24 @@ def characterCreation():
     return character
 
 #Criação de monstros aleatorios
-monsters = []
-for i in range(1,10):
-
-    health=random.randint(100,105) #Vida do monstro
-    attack=random.randint(1,2) #Ataque do monstro
-    defense=random.randint(0,1) #Defesa do monstro
-    souls=random.randint(10, 20) #Almas que o monstro dropa
-    speed=random.randint(1,2) #Velocidade do monstro
-
-    monster = monstersCreation.monsterTypeA(health, attack, defense, speed, souls)
-    monsters.append(monster)
 
 
-    # Exibir todas informações do monstro
-    print(f"""
-    x---------------------------------------------x
-    |                                             |
-    |               Monstro {i}                    |
-    |                                             |
-    x---------------------------------------------x
-    |                                             |
-    | Vida: {health}                               |
-    | Ataque: {attack}                             |
-    | Defesa: {defense}                            |
-    | Velocidade: {speed}                          |
-    | Almas: {souls}                               |
-    |                                             |
-    x---------------------------------------------x
-    """)
+def monstersCreationFunction():
+    monsters = []
+    for i in range(9):
 
+        health=random.randint(100,105) #Vida do monstro
+        attack=random.randint(1,2) #Ataque do monstro
+        defense=random.randint(0,1) #Defesa do monstro
+        souls=random.randint(10, 20) #Almas que o monstro dropa
+        speed=random.randint(1,2) #Velocidade do monstro
+
+        monster = monstersCreation.monsterTypeA(health, attack, defense, speed, souls)
+        monsters.append(monster)
+
+    return monsters
+    
+monsters = monstersCreationFunction()
+for monster in monsters:
+    monster.showMonsterStats()
+print(monsters)
--- monstersCreation.py
+++ monstersCreation.py
@@ -1,8 +1,23 @@
 class monsterTypeA():
 
     def __init__(self, health, attack, defense, speed, souls):
+
         self.health = health
         self.attack = attack
         self.defense = defense
         self.speed = speed
         self.souls = souls
+    
+
+    def showMonsterStats(self):
+        print(f"""
+        |---------------------------------------|
+        |                                       |
+        |   Vida: {self.health}                     |
+        |   Ataque: {self.attack}                   |
+        |   Defesa: {self.defense}                   |
+        |   Velocidade: {self.speed}                |
+        |   Almas: {self.souls}                      |
+        |                                       |
+        |---------------------------------------|
+        """)
