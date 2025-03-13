7ba350e41646c942010fc59f08a6e5cd7c8d0796
mkviniciuz <54824074+mkviniciuz@users.noreply.github.com>
2025-03-13 18:00:02 -0300

    Desenvolvimento de eventos bons para o player

--- main.py
+++ main.py
@@ -4,6 +4,48 @@ from monstersCreation import character
 import time
 
 
+killcount = 0
+floor_level = 1
+good_events = {
+
+                #Almas encontradas
+                "1": "Você encontrou um baú de tesouro! Você ganhou 50 almas!",
+                "2": "Você encontrou um baú de tesouro! Você ganhou 100 almas!",
+                "3": "Você encontrou um baú de tesouro! Você ganhou 150 almas!",
+                "4": "Você encontrou um baú de tesouro! Você ganhou 200 almas!",
+                "5": "Você encontrou um baú de tesouro! Você ganhou 250 almas!",
+                "6": "Você encontrou um baú de tesouro! Você ganhou 300 almas!",
+                "7": "Você encontrou um baú de tesouro! Você ganhou 350 almas!",
+                "8": "Você encontrou um baú de tesouro! Você ganhou 400 almas!",
+                "9": "Você encontrou um baú de tesouro! Você ganhou 450 almas!",
+                "10": "Você encontrou um baú de tesouro! Você ganhou 500 almas!"
+
+                #Alterações de status
+                "11": "Os deuses estão ao seu favor! Você ganhou +10 de ataque!",
+                "12": "Os deuses estão ao seu favor! Você ganhou +10 de defesa!",
+                "13": "Os deuses estão ao seu favor! Você ganhou +10 de vida!",
+                "14": "Os deuses estão ao seu favor! Você ganhou +10 de velocidade!",
+                "15": "Os deuses estão ao seu favor! Você ganhou +20 de ataque!",
+                "16": "Os deuses estão ao seu favor! Você ganhou +20 de defesa!",
+                "17": "Os deuses estão ao seu favor! Você ganhou +20 de vida!",
+                "18": "Os deuses estão ao seu favor! Você ganhou +20 de velocidade!",
+                "19": "Os deuses estão ao seu favor! Você ganhou +30 de ataque!",
+                "20": "Os deuses estão ao seu favor! Você ganhou +30 de defesa!",
+
+                #Debuff em monstros
+                "21": "Você encontrou um pergaminho de maldição! O monstro perdeu 10 de ataque!",
+                "22": "Você encontrou um pergaminho de maldição! O monstro perdeu 10 de defesa!",
+                "23": "Você encontrou um pergaminho de maldição! O monstro perdeu 10 de vida!",
+                "24": "Você encontrou um pergaminho de maldição! O monstro perdeu 10 de velocidade!",
+                "25": "Você encontrou um pergaminho de maldição! O monstro perdeu 20 de ataque!",
+                "26": "Você encontrou um pergaminho de maldição! O monstro perdeu 20 de defesa!",
+                "27": "Você encontrou um pergaminho de maldição! O monstro perdeu 20 de vida!",
+                "28": "Você encontrou um pergaminho de maldição! O monstro perdeu 20 de velocidade!",
+                "29": "Você encontrou um pergaminho de maldição! O monstro perdeu 30 de ataque!",
+                "30": "Você encontrou um pergaminho de maldição! O monstro perdeu 30 de defesa!"
+                }
+
+
 #Criação de monstros aleatorios
 def monstersCreationFunction():
     for i in range(1):
@@ -41,16 +83,6 @@ def main():
             """)
 
             time.sleep(2)
-
-            print(f"""
-
-            x STATUS do MONSTRO!x
-            |
-            |  --> Vida: {monster.health}
-            |  --> Ataque: {monster.attack}
-            |  --> Defesa: {monster.defense}
-            |  --> Almas: {monster.souls}
-                """) #Exibir status do primeiro monstro!
 
         except ValueError:
             print("Você morreu!")
